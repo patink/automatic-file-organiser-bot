6214467697a2e6dcf7a8239b88b4e2a5f3fd4f84
Nisith Pati <54003224+patink@users.noreply.github.com>
2021-06-12 16:55:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import os 
+import shutil
+
+image_formats = ["jpg","png","jpeg","gif","webp","tiff"]
+audio_formats = ["mp3","wav"]
+video_formats = ["mp4","avi","webm"]
+docs_formats = ["ai","ait","txt","rtf","docx","pdf","pptx"]
+
+while True:
+
+    files = os.listdir("./")
+
+    for file in files:
+        if os.path.isfile(file) and file != "app.py":
+            ext = (file.split(".")[-1]).lower()
+
+            if ext in image_formats:
+                shutil.move(file,"images/"+file)
+            elif ext in audio_formats:
+                shutil.move(file,"audio/"+file)
+            elif ext in video_formats:
+                shutil.move(file,"videos/"+file)
+            elif ext in docs_formats:
+                shutil.move(file,"docs/"+file)
+            else:
+                shutil.move(file,"others/"+file)
